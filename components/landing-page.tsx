82feb711e341e48c368b166620c750a68b76060c
unconch <unconch@users.noreply.github.com>
2026-03-10 11:52:24 +0530

    Add use client to landing-page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import dynamic from "next/dynamic"
 import { HeroSection } from "@/components/landing-page/HeroSection"
 import { LandingPageClient } from "@/components/landing-page-client"
