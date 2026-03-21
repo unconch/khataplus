badcfe5c1f3ab3c6ef765f0bf6dbedfd075a4556
unconch <unconch@users.noreply.github.com>
2026-03-21 19:50:23 +0530

    Remove Sentry files and instrumentation

--- instrumentation-client.ts
+++
@@ -1,4 +0,0 @@
-import * as Sentry from "@sentry/nextjs";
-
-// Hook required by Sentry to instrument router transitions.
-export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
